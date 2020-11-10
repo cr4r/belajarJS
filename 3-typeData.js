9c8bdbc2b40ad095f8918c20cdc2874c8aa31eb5
cr4r <ahmadfatchurrachman@gmail.com>
2020-11-10 09:20:08 -0500

    Update

---
+++ 3-typeData.js
@@ -0,0 +1,44 @@
+/*Tipe data adalah klasifikasi yang kami berikan untuk berbagai jenis data yang kami gunakan dalam pemrograman.
+Di JavaScript, ada tujuh tipe data dasar:
+Integer: Angka apa pun, termasuk angka dengan desimal: 4, 8, 1516, 23.42.
+String: Pengelompokan karakter apa pun pada keyboard Anda (huruf, angka, spasi, simbol, dll.) Yang diapit oleh tanda kutip tunggal: '...' atau tanda kutip ganda "...". Padahal kami lebih suka tanda kutip tunggal.
+        Beberapa orang suka menganggap string sebagai kata mewah untuk teks.
+Boolean: Tipe data ini hanya memiliki dua kemungkinan nilai - "true" atau "false" (tanpa tanda kutip).
+        Sangat membantu untuk menganggap boolean sebagai tombol "on" dan "off" atau sebagai jawaban untuk pertanyaan "ya" atau "tidak".
+Null: Tipe data ini mewakili nilai yang tidak ada secara sengaja, dan diwakili oleh keyword "null" (tanpa tanda kutip).
+Undefined: Tipe data ini dilambangkan dengan keyword "undefined" (tanpa tanda kutip).
+        Ini juga mewakili tidak adanya nilai meskipun memiliki penggunaan yang berbeda dari null.
+Symbol: Fitur bahasa yang lebih baru, simbol adalah pengenal unik, berguna dalam pengkodean yang lebih kompleks.
+        Tidak perlu khawatir tentang ini untuk saat ini.
+Objek: Koleksi data terkait.*/
+
+console.log('Dibawah ini type data Integer')
+console.log('Kode: console.log(2011)')
+console.log(2011)
+
+console.log('\nDibawah ini type data String')
+console.log('Kode: console.log(\'Ini string\')')
+console.log('Ini string')
+
+console.log('\nDibawah ini type data undifined')
+console.log('Kode: console.log(undefined)')
+console.log(undefined)
+
+console.log('\nDibawah ini type data null')
+console.log('Kode: console.log(null)')
+console.log(null)
+
+console.log('\nDibawah ini type data Bolean')
+console.log('Kode: console.log(1==1)')
+console.log(1==1)
+
+console.log('\n\n//Bonus')
+console.log('kode: console.log(\'\"Hai kak\"\')')
+console.log('\"Hai kak\"')
+
+console.log('\nkode: console.log(20.3040)')
+console.log(20.3040);
+
+a = 'Halo ini variabel a'
+console.log('\nkode: a = \'Halo ini variabel a\'\nconsole.log(${a})')
+console.log(`${a}`)
---
+++ 4-aritmatika.js
@@ -0,0 +1,27 @@
+/*Operator Aritmatika
+Aritmatika dasar sering kali berguna saat pemrograman.
+
+Operator adalah karakter yang melakukan tugas dalam kode kita.
+JavaScript memiliki beberapa operator aritmatika bawaan, yang memungkinkan kita melakukan kalkulasi matematis pada angka.
+Ini termasuk operator berikut dan simbolnya yang sesuai:
+
+Tambahkan: +
+Kurangi: -
+Kalikan: *
+Bagi: /
+Sisa:%
+Empat yang pertama berfungsi seperti yang Anda duga:
+console.log(3 + 4); // Hasil 7
+console.log(5 - 1); // Hasil 4
+console.log(4 * 2); // Hasil 8
+console.log(9 / 3); // Hasil 3
+console.log(11 % 3) // Hasil 2*/
+
+console.log('Kode: console.log(19+3.5)')
+console.log(19+3.5)
+console.log('\nKode: console.log(2020-1969)')
+console.log(2020-1969)
+console.log('\nKode: console.log(65/240)')
+console.log(65/240)
+console.log('\nKode: console.log(0.2708*100)')
+console.log(0.2708*100)
